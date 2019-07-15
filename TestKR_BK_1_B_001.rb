cb8a3958ba4c1a6b6566c5340607b42f3743e20b
skcrackers <skcrackers@gmail.com>
2019-07-15 18:31:00 +0900

    작성중

--- TestKR_BK_1_B_001.rb
+++ TestKR_BK_1_B_001.rb
@@ -17,6 +17,7 @@ def http_sender(url, token, body)
   }
 
   http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
   request = Net::HTTP::Post.new(url, headers)
   request.body = body.to_json
   response = http.request(request)
@@ -33,14 +34,14 @@ def request_token(url, client_id, client_secret)
   uri = URI.parse(url)
 
   authHeader = Base64.encode64(client_id + ':' + client_secret)
-
   headers = {
-    'Acceppt': 'application/json',
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Authorization': 'Basic ' + authHeader
+    'Accept'=> 'application/json',
+    'Content-Type'=> 'application/x-www-form-urlencoded',
+    'Authorization'=> 'Basic ' + authHeader
   }
 
   http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
   request = Net::HTTP::Post.new(url, headers)
   request.body = 'grant_type=client_credentials&scope=read'
   response = http.request(request)
@@ -53,16 +54,14 @@ end
 response = nil
 
 # CodefURL
-codef_url = 'http://192.168.10.126:10001'
-token_url = 'http://192.168.10.126:8888/oauth/token'
+codef_url = 'https://tapi.codef.io'
+token_url = 'https://toauth.codef.io/oauth/token'
 
 # 은행 법인 보유계좌
 account_list_path = '/v1/kr/bank/b/account/account-list'
 
 # 기 발급된 토큰
 token ='eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXJ2aWNlX3R5cGUiOiIwIiwic2NvcGUiOlsicmVhZCJdLCJzZXJ2aWNlX25vIjoiOTk5OTk5OTk5OSIsImV4cCI6MTU2MjczNjUyNywiYXV0aG9yaXRpZXMiOlsiSU5TVVJBTkNFIiwiUFVCTElDIiwiQkFOSyIsIkVUQyIsIlNUT0NLIiwiQ0FSRCJdLCJqdGkiOiIyODBhNjVkOS02NjU1LTQ5MzYtODEwNS05MjUyYTk4MGRjMDgiLCJjbGllbnRfaWQiOiJjb2RlZl9tYXN0ZXIifQ.eFCEgxcntsEkjFORAWGSi6949UMOuCxVsm2wnYlDXqrHWXXwG7-XfKugsBNone_qRRGeKD3iv6f_TEcVMWyTz8aS0fRbE514LVz6PnzKbruyPNDA5Pk3ym8up9h4Ba1ix__Bvpu_TB0Y7Fikk9YHWHacJy4F_WOjr8xFP-q2egh763_LqVUzRakGQoLOTukduZ5zH5lfSO1Z9yx2cnDkY4VSM9DTbycSZuA2oQkMVpXJc0slEyWLw7WNX5E-ff3fL6ePfJvu7by_4KmgmmJkOoKBWvJ00DwrwhAa1EZmjqGPYG6RE6wxSwsu3lYeiCX-jSGm_cbKdk7YDnYxm8FKzg'
-
-token = 'a'
 
 # BodyData
 body = {
@@ -73,8 +72,10 @@ body = {
 # CODEF API 요청
 response_codef_api = http_sender(codef_url + account_list_path, token, body)
 
+if response_codef_api.code == '200'
+  puts('정상처리')
 # token error
-if response_codef_api.code == '401'
+elsif response_codef_api.code == '401'
     dict = JSON.parse(response_codef_api.body)
 
     # invalid_token
@@ -85,6 +86,7 @@ if response_codef_api.code == '401'
     # reissue token
     response_oauth = request_token(token_url, "codef_master", "codef_master_secret");
     puts('response_oauth.code = ' + response_oauth.code)
+    puts('response_oauth.code = ' + response_oauth.body)
     if response_oauth.code == '200'
         dict = JSON.parse(response_oauth.body)
 
